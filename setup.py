cfdbe6d3b8d60ac691dd9176c899f4b23b44ac28
algorythmist <dimitri@tecacet.com>
2021-06-12 09:17:45 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='computational-physics',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/algorythmist/computational-physics',
+    license='MIT',
+    author='Dimitri Papaioannou',
+    author_email='dimitri@tecacet.com',
+    description='Financial Toolbox',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3'
+)
